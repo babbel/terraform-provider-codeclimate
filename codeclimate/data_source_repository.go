efc78ac0ce21c291dddf7af2b08d96856b6d82b5
Kirill Zonov <kzonov@babbel.com>
2019-05-06 14:46:41 +0200

    Extracted CC call into a separate function

--- codeclimate/data_source_repository.go
+++ codeclimate/data_source_repository.go
@@ -41,27 +41,41 @@ func dataSourceRepository() *schema.Resource {
 }
 
 func dataSourceRepositoryRead(d *schema.ResourceData, m interface{}) error {
-	var repositoryData ReadRepositoryResponse
-
-	// TODO: Extract into a client
-	client := &http.Client{}
 	repoId := d.Get("repository_id").(string)
-
-	req, err := http.NewRequest("GET", fmt.Sprintf("%s/repos/%s", codeClimateApiHost, repoId), nil)
-
+	repositoryData, err := getRepository(m.(Config).apiKey, repoId)
 	if err != nil {
 		log.Println(err)
 		return err
 	}
 
+	d.SetId(repositoryData.(ReadRepositoryResponse).Data.ID)
+	// TODO: Check that repositoryData.Data.Attributes.TestReporterID exists
+	d.Set("test_reporter_id", repositoryData.(ReadRepositoryResponse).Data.Attributes.TestReporterID)
+
+	return err
+}
+
+func getRepository(apiKey string, repoId string) (interface{}, error) {
+	var repositoryData ReadRepositoryResponse
+
+	// TODO: Extract into a client
+	client := &http.Client{}
+
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s/repos/%s", codeClimateApiHost, repoId), nil)
+
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
 	req.Header.Add("Accept", `W/"application/vnd.api+json"`)
-	req.Header.Add("Authorization", fmt.Sprintf("Token token=%s", m.(Config).apiKey))
+	req.Header.Add("Authorization", fmt.Sprintf("Token token=%s", apiKey))
 
 	resp, err := client.Do(req)
 
 	if err != nil {
 		log.Println(err)
-		return err
+		return nil, err
 	}
 
 	defer resp.Body.Close()
@@ -69,19 +83,14 @@ func dataSourceRepositoryRead(d *schema.ResourceData, m interface{}) error {
 
 	if err != nil {
 		log.Println(err)
-		return err
+		return nil, err
 	}
 
 	err = json.Unmarshal(data, &repositoryData)
-
 	if err != nil {
 		log.Println(err)
-		return err
+		return nil, err
 	}
 
-	d.SetId(repositoryData.Data.ID)
-	// TODO: Check that repositoryData.Data.Attributes.TestReporterID exists
-	d.Set("test_reporter_id", repositoryData.Data.Attributes.TestReporterID)
-
-	return err
+	return repositoryData, nil
 }
